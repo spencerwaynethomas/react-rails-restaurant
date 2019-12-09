04b7712e1ee8492390da6c4ab0987a8dc605e97f
Spencer Thomas <spencerthomas@Spencers-MacBook-Air.local>
2019-12-09 16:16:48 -0700

    WIP

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,38 @@
 import React, { Component, } from 'react';
 import './App.css';
 import { Container, } from "semantic-ui-react"
+import MenuForm from "./components/MenuForm";
 
 class App extends Component {
-  state = { menus: [], }
+  state = { menus: [], };
 
   componentDidMount() {
     // make a call to rails for mwnu
-  }
+  };
 
   addMenu = (name) => {
     // make api call to add menu
-  }
+  };
 
   updateMenu = (id) => {
     // make api call to update menu
     // update state
-  }
+  };
 
   deleteMenu = (id) => {
     // make api call to delete menu
-  }
+  };
 
   render() {
     return (
       <Container style={{ padding: "30px 0" }}>
+        <MenuForm addMenu={this.addMenu}/>
+        <br />
+        <br />
         <h1>React Restaurants</h1>
       </Container>
-    )
-  }
-}
+    );
+  };
+};
 
 export default App;
